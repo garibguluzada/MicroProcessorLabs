896760d0168c5d1a0b7c7317066c4f6dcb3ad4fe
Garib Guluzada <118480971+garibguluzada@users.noreply.github.com>
2025-10-21 14:50:23 +0400

    Update sketch_sep11c.ino

--- 1/1.2/sketch_sep11c.ino
+++ 1/1.2/sketch_sep11c.ino
@@ -1,18 +1,10 @@
 void setup() {
-  // put your setup code here, to run once:
   DDRB |= (1 << DDB5);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- PORTB |= (1 << PORTB5);
- delay(500);
-
- PORTB &= ~(1<<PORTB5);
- delay(500);
-}
-//maximum bytes =2048   storgae space =640 byte,Maximum is 32256 bytes.
-
-
-//Sketch uses 924 bytes (2%) of program storage space. Maximum is 32256 bytes.
-//Global variables use 9 bytes (0%) of dynamic memory, leaving 2039 bytes for local variables. Maximum is 2048 bytes.
+  PORTB |= (1 << PORTB5);
+  delay(500);
+  PORTB &= ~(1 << PORTB5);
+  delay(500);
+} 
