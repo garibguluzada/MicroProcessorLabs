42682f164a95979d70c7adcf9555bc7076bee2da
Garib Guluzada <118480971+garibguluzada@users.noreply.github.com>
2025-10-27 00:54:48 +0400

    Correction on Lab2.2

--- Lab 2/2.2/sketch_sep18b.ino
+++ Lab 2/2.2/sketch_sep18b.ino
@@ -1,15 +1,20 @@
 void setup() {
   Serial.begin(9600);
+  uint8_t result;
 
-  uint8_t R12 = 5;
-  uint8_t R13 = 8;
-  uint8_t R18;
-
-  R18 = R12 + R13;
+  asm volatile(
+    "ldi r12, 5      \n\t"
+    "ldi r13, 8      \n\t"
+    "mov r18, r12    \n\t"
+    "add r18, r13    \n\t"
+    "sts %[res], r18 \n\t"   // store r18 into 'result' variable
+    : [res] "=m" (result)    // output
+    :                        // no input
+    : "r12", "r13", "r18"    // clobbered registers
+  );
 
   Serial.print("Result (R18) = ");
-  Serial.println(R18);
+  Serial.println(result);   // should print 13
 }
 
-void loop() {
-}
+void loop() {}
